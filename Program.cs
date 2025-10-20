7d4512ae13239947aa73ee88c788bc0b8e227246
Marthexo <marta.kawik00@gmail.com>
2025-10-21 00:08:44 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Hello, Mariusz!");
+        // Console.WriteLine($"Mariusz jest {args[0]}");
+        Console.WriteLine($"Sumą 2+2 jest {2 + 2}");
+    }
+}
